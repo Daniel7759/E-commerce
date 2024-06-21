53e01d652e7fa98fff5010a31f06564a9b37e115
Daniel7759 <danieljh7759@gmail.com>
2024-06-21 15:41:03 -0500

    Cambiamos la conexion de Firebase a variables de entorno y eliminamos el certificado autofirmado.

--- src/main/java/com/example/configurations/FirebaseConfig.java
+++ src/main/java/com/example/configurations/FirebaseConfig.java
@@ -9,21 +9,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
-    @Value("${firebase.config.path}")
-    private String firebaseConfigPath;
+    /*@Value("${firebase.config.path}")
+    private String firebaseConfigPath;*/
 
     @Bean
     public FirebaseApp initializeFirebase() {
         try {
-            InputStream serviceAccount = new ClassPathResource(firebaseConfigPath).getInputStream();
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(getFirebaseConfig()))
                     .setStorageBucket("ecommerce-ds-b10f0.appspot.com")
                     .build();
             return FirebaseApp.initializeApp(options);
@@ -31,5 +31,33 @@ public class FirebaseConfig {
             e.printStackTrace();
             throw new RuntimeException("Failed to initialize Firebase", e);
         }
+    }
+
+    private InputStream getFirebaseConfig() {
+        String firebaseConfig = String.format(
+                "{\n" +
+                        "  \"type\": \"%s\",\n" +
+                        "  \"project_id\": \"%s\",\n" +
+                        "  \"private_key_id\": \"%s\",\n" +
+                        "  \"private_key\": \"%s\",\n" +
+                        "  \"client_email\": \"%s\",\n" +
+                        "  \"client_id\": \"%s\",\n" +
+                        "  \"auth_uri\": \"%s\",\n" +
+                        "  \"token_uri\": \"%s\",\n" +
+                        "  \"auth_provider_x509_cert_url\": \"%s\",\n" +
+                        "  \"client_x509_cert_url\": \"%s\"\n" +
+                        "}",
+                System.getenv("TYPE"),
+                System.getenv("PROJECT_ID"),
+                System.getenv("PRIVATE_KEY_ID"),
+                System.getenv("PRIVATE_KEY").replace("\\n", "\n"),
+                System.getenv("CLIENT_EMAIL"),
+                System.getenv("CLIENT_ID"),
+                System.getenv("AUTH_URI"),
+                System.getenv("TOKEN_URI"),
+                System.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+                System.getenv("CLIENT_X509_CERT_URL")
+        );
+        return new ByteArrayInputStream(firebaseConfig.getBytes());
     }
 }
