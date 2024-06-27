803bd59a264b3172d1ce9b122079a5347eb9e8dd
Daniel7759 <danieljh7759@gmail.com>
2024-06-27 14:37:50 -0500

    agregamos la ruta de origen del front a la configuracion de cors

--- src/main/java/com/example/configurations/WebConfig.java
+++ src/main/java/com/example/configurations/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://localhost:3000","https://e-comerce-proyect-rmr2193pl-samirs-projects-87dbd559.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
